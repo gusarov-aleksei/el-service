a30cdf94f048526139caec29ec120aaf964bc4eb
gusaroval <gusarov.aleksey@gmail.com>
2021-09-22 14:02:25 +0300

    script for setup VM instance, firewall rule notes about GCP

---
+++ gcp/init_gcp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# stop script in case of command error
+set -eou pipefail
+
+# set default compute/region and compute/zone
+gcloud config set compute/region us-east1
+gcloud config set compute/zone us-east1-b
+
+# create VM with container and pass environment variable
+gcloud compute instances create-with-container el-service-vm --machine-type e2-micro --container-image us-east1-docker.pkg.dev/algus-project-382/algus-docker-repo/el-service:tag1 --container-env STORAGE_TYPE=GS
+
+# add tag to el-service-vm
+gcloud compute instances add-tags el-service-vm --tags el-service-tag
+
+# open port and and connect it to el-service-vm
+gcloud compute firewall-rules create el-service-traffic --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:8080 --source-ranges=0.0.0.0/0 --target-tags=el-servie-tag
