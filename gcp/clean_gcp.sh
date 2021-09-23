7af0718e27c1b9df8d990889e4a16eb00213ae6e
gusaroval <gusarov.aleksey@gmail.com>
2021-09-23 10:09:33 +0300

    delete resources script

---
+++ gcp/clean_gcp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#clean GCP  resources
+
+# stop script in case of command error
+set -eou pipefail
+
+echo "*** Delete resources of network"
+gcloud compute firewall-rules delete el-service-traffic
+
+echo "*** Delete resources of compute engine"
+gcloud compute instances delete el-service-vm
+
+echo "*** Delete resources of storage"
+gsutil rm -r gs://algus-bucket/**
--- gcp/init_gcp.sh
+++ gcp/init_gcp.sh
@@ -3,15 +3,18 @@
 # stop script in case of command error
 set -eou pipefail
 
+echo "*** Set project properties"
 # set default compute/region and compute/zone
 gcloud config set compute/region us-east1
 gcloud config set compute/zone us-east1-b
 
 # create VM with container and pass environment variable
+echo "*** Create resources of compute"
 gcloud compute instances create-with-container el-service-vm --machine-type e2-micro --container-image us-east1-docker.pkg.dev/algus-project-382/algus-docker-repo/el-service:tag1 --container-env STORAGE_TYPE=GS
 
 # add tag to el-service-vm
 gcloud compute instances add-tags el-service-vm --tags el-service-tag
 
 # open port and and connect it to el-service-vm
+echo "*** Create resources of network"
 gcloud compute firewall-rules create el-service-traffic --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:8080 --source-ranges=0.0.0.0/0 --target-tags=el-service-tag
