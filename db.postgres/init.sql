6985f214bc60e2c78468e2fad530c72e5983a603
gusaroval <gusarov.aleksey@gmail.com>
2021-12-17 02:31:42 +0300

    DB schema setup

---
+++ db.postgres/init.sql
@@ -0,0 +1,8 @@
+CREATE SCHEMA IF NOT EXISTS el_service AUTHORIZATION postgres;
+SET SCHEMA 'el_service';
+
+CREATE TABLE IF NOT EXISTS podcast (
+    id SMALLSERIAL NOT NULL PRIMARY KEY,
+    metadata jsonb,
+    body json
+);
